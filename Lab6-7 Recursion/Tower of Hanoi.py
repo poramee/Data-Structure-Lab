7cd1dc9f9b830fdf4e38400d9c9c52438559d3db
poramee <poramee.chansuksett@gmail.com>
2019-10-15 13:35:27 +0700

    remove unnecessary code in Tower of Hanoi.py

--- Lab6-7 Recursion/Tower of Hanoi.py
+++ Lab6-7 Recursion/Tower of Hanoi.py
@@ -6,24 +6,5 @@ def towerOfHanoi(n,fr,to,ax):
         print("move",n,"from",fr,"to",to) # From A to C
         towerOfHanoi(n - 1,ax,to,fr) # From B to C
 
-
-
-
-
-
-
-
-
-dp = [None] * 100000
-def fibo(n):
-    if n <= 1:
-        dp[n] = n
-        return n
-    elif dp[n] != None:
-        return dp[n]
-    else:
-        dp[n] = fibo(n-1) + fibo(n-2)
-        return dp[n]
-
 towerOfHanoi(2,'A','C','B')
 # print(fibo(100))
