abf09ec53bcb0286958394886e7231903607305e
poramee <poramee.chansuksett@gmail.com>
2019-10-19 10:36:37 +0700

    change knapsack algorithm

--- Lab6-7 Recursion/Knapsack.py
+++ Lab6-7 Recursion/Knapsack.py
@@ -1,14 +1,15 @@
 def knapsack(weight,availableList,inBagList):
     if weight == 0:
-        print(">> ",inBagList)
-        return 0
-    elif weight < 0:
-        return 0
-    else:
-        for i in range(0,len(availableList)):
-            tempInBag = []
-            tempInBag += inBagList
-            tempInBag.append(availableList[i])
-            knapsack(weight - availableList[i], availableList[(i+1):], tempInBag)
+        print(inBagList)
+        return
+    if weight < 0 or len(availableList) == 0:
+        return
+
+    temp = []
+    temp += inBagList
+    temp.append(availableList[0])
+
+    knapsack(weight - availableList[0],availableList[1:],temp)
+    knapsack(weight,availableList[1:],inBagList)
 
 knapsack(20, [20, 10, 5, 5, 3, 2, 20, 10],[])
