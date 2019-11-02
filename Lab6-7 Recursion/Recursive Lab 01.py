c5c77631442d15528c06b2187ba61e370f432912
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2019-11-02 20:05:26 +0700

    change recursive lab

--- Lab6-7 Recursion/Recursive Lab 01.py
+++ Lab6-7 Recursion/Recursive Lab 01.py
@@ -1,87 +1,169 @@
+import random
+
+
 def fac(n):
-    if n == 1:
+    if n == 0 or n == 1:
         return 1
     return n * fac(n - 1)
 
-def sum1ToN(n):
-    if n == 1:
-        return 1
-    return n + sum1ToN(n - 1)
 
-def printNTo1(n):
-    print(n)
-    if n > 1: printNTo1(n - 1)
+print("fac(5) = ", fac(5))
+
+
+def sum1ToN(n):
+    if n < 0:
+        return 0
+    return n + sum1ToN(n-1)
+
+
+print("sum1ToN(5) = ", sum1ToN(5))
+
+
+def printNto1(n):
+    if n < 1:
+        print("")
+        return
+    print(n, end=" ")
+    printNto1(n - 1)
+
+
+print("printNto1(5) = ", end="")
+printNto1(5)
+
 
 def print1ToN(n):
-    if n > 1: print1ToN(n - 1)
-    print(n)
+    if n <= 1:
+        print(n, end=" ")
+    else:
+        print1ToN(n - 1)
+        print(n, end=" ")
+
+
+print("print1toN(5) = ", end="")
+print1ToN(5)
+print("")
+
 
 def fib(n):
     if n <= 1:
         return n
-    return fib(n - 1) + fib(n - 2)
+    else:
+        return fib(n - 1) + fib(n - 2)
 
-def binarySearch(lo,hi,x,l):
+
+print("fib(5) = ", fib(5))
+
+
+def binarySearch(lo, hi, x, l):
     index = int((lo + hi) / 2)
-
     if lo > hi:
         return None
-    elif l[index] > x:
-        return binarySearch(lo,index - 1,x,l)
-    elif l[index] < x:
-        return binarySearch(index + 1,hi,x,l)
+    if x < l[index]:
+        return binarySearch(lo, index - 1, x, l)
+    elif x > l[index]:
+        return binarySearch(index + 1, hi, x, l)
     else:
         return index
 
-def move(n,fr,to,ax):
-    if n == 1:
-        print("move",n,"from",fr,"to",to)
-    else:
-        move(n - 1,fr,ax,to)
-        print("move", n, "from", fr, "to", ax)
-        move(n - 1,ax,to,fr)
 
-def sum1(n,l):
+temp = []
+for i in range(0, 20):
+    temp.append(random.randint(0, 1000))
+temp.sort()
+print(temp)
+print("binarySearch(", temp[18], ") = ",
+      binarySearch(0, 19, temp[18], temp), sep="")
+print("binarySearch(", 2000, ") = ",
+      binarySearch(0, 19, 2000, temp), sep="")
+
+
+def move(n, fr, to, ax):
+    if n == 1:
+        print("move ", n, " : ", fr, " -> ", to)
+    else:
+        move(n - 1, fr, ax, to)
+        print("move ", n, " : ", fr, " -> ", to)
+        move(n - 1, ax, to, fr)
+
+
+print("move: ")
+move(3, 'A', 'C', 'B')
+
+
+def sum1(n, l):
     if n == 0:
         return l[0]
     else:
-        return l[n] + sum1(n - 1,l)
+        return l[n] + sum1(n - 1, l)
 
-def sum2(l,i = 0):
+
+print("sum1(19,temp) = ", sum1(19, temp))
+
+
+def sum2(l, i=0):
     if i == len(l):
         return 0
     else:
-        return l[i] + sum2(l,i + 1)
+        return l[i] + sum2(l, i+1)
+
+
+print("sum2(temp) = ", sum2(temp))
+
 
 def sum3(l):
-    if len(l) == 1:
-        return l[0]
-    return l[0] + sum3(l[1:])
+    if len(l) == 0:
+        return 0
+    else:
+        return l[0] + sum3(l[1:])
+
+
+print("sum3(temp) = ", sum3(temp))
+
 
 def printListForw(l):
-    print(l[0])
-    if(len(l) > 1): printListForw(l[1:])
+    if len(l) > 0:
+        print(l[0], end=" ")
+        printListForw(l[1:])
+
+
+print("printListForw(temp) = ")
+printListForw(temp)
+print("")
+
 
 def printListBkw(l):
-    if(len(l) > 1):
+    if len(l) > 1:
         printListBkw(l[1:])
-    print(l[0])
+    print(l[0], end=" ")
 
-def app(n,l):
-    if n == 0:
-        return l
-    else:
-        ll = app(n - 1,l)
-        ll.append(n)
-        return ll
 
-def appN(n,l):
+print("printListBkw(temp) = ")
+printListBkw(temp)
+print("")
+
+
+def app(n, l):
     if n == 0:
-        return l
+        return None
+
+    if n > 1:
+        app(n - 1, l)
     l.append(n)
-    return appN(n - 1,l)
 
-# LINKED LIST
+
+    # return l
+print("app(10,[]) = ", app(2, []))
+
+
+def appB(n, l):
+    if n >= 1:
+        l.append(n)
+        return appB(n - 1, l)
+    return l
+
+
+print("appB(10,[]) = ", appB(10, []))
+
 
 class node:
     def __init__(self, d, nxt=None):
@@ -90,53 +172,55 @@ class node:
             self.next = None
         else:
             self.next = nxt
-class List:
-    def __init__(self,head = None):
-        self.head = head
+
+
+h = node(1)
+currNode = h
+for i in range(2, 10):
+    currNode.next = node(i)
+    currNode = currNode.next
+
 
 def printList(h):
-    if h == None:
-        return 0
-    print(h.data)
-    printList(h.next)
+    if h is not None:
+        print(h.data, end=" ")
+        printList(h.next)
 
-def createLLL(head,list):
-    if len(list) == 0:
-        return head
-    elif head == None:
-        head = node(list[0])
-        createLLL(head,list[1:])
+
+print("printList(h)")
+printList(h)
+print()
+
+
+def createLLL(l):
+    # if len(l) == 0:
+    #     return None
+    # h = node(l[0])
+    # h.next = createLLL(l[1:])
+    # return h
+    if len(l) == 0:
+        return None
+    else:
+        nx = createLLL(l[1:])
+        h = node(l[0])
+        h.next = nx
+        return h
+
+
+print("createLLL(['A','B','C','D'])")
+printList(createLLL(['A', 'B', 'C', 'D']))
+print()
+
+
+def createLL(n, head=None):
+    if n == 0:
         return head
     else:
-        head.next = node(list[0])
-        createLLL(head.next,list[1:])
-        return head
-
-def createLL(head,n):
-    if head == None:
-        head = node(n)
-        createLL(head,n - 1)
-    elif n > 0:
-        head.next = node(n)
-        createLL(head.next,n - 1)
-    return head
+        newHead = node(n)
+        newHead.next = head
+        return createLL(n - 1, newHead)
 
 
-
-
-l = [0,1,2,3,4,5]
-print(binarySearch(0,5,2.5,l))
-
-# print(sum2(l))
-# print(sum3(l))
-# printListBkw(l)
-# print(appN(5,l))
-
-# linkedList = List()
-
-# linkedList.head = createLL(linkedList.head,5)
-# printList(linkedList.head)
-
-# linkedList.head = createLLL(linkedList.head,l)
-# currNode = linkedList.head
-# printList(linkedList.head)
+print("createLL(10)")
+printList(createLL(10))
+print()
