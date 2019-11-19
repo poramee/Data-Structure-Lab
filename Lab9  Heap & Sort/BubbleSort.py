8edd600d1ff884a3106a662daeaa72f7c6381810
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2019-11-19 12:40:55 +0700

    Add Compare Counter in BubbleSort.py

--- Lab9  Heap & Sort/BubbleSort.py
+++ Lab9  Heap & Sort/BubbleSort.py
@@ -9,10 +9,13 @@ def isSorted(list):
 
 def bubbleSort(list):
     length = len(list)
+    comp = 0
     for i in range(0, length - 1):
         for j in range(0, length - i - 1):
+            comp += 1
             if list[j] > list[j+1]:
-                list[j].data, list[j+1].data = list[j+1].data, list[j].data
+                list[j].data, list[j + 1].data = list[j + 1].data, list[j].data
+    print("Total Compare: ",comp)
     return list
 
 l = LinkedList()
